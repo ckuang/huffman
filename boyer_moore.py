e624ea9fe8fafc088068ef7e044ce4a5e898cb15
ckuang <ckuang8@gmail.com>
2018-10-23 16:09:32 -0400

    added boyer moore

--- boyer_moore.py
+++ boyer_moore.py
@@ -1,12 +1,3 @@
-# Implementation of the Boyer-Moore majority vote algorithm.
-#
-# Given a list of elements `l`, one would use this class in the following way
-# to get the element that appears the majority of the time in the list:
-#
-# b = BoyerMooreMajority()
-# for elem in l:
-#   b.add_next_element(elem)
-# return b.get_majority()
 class BoyerMooreMajority:
   def __init__(self):
     self.guess = None
@@ -18,10 +9,27 @@ class BoyerMooreMajority:
     assert(element is not None)
     #YOUR CODE HERE
 
+    if self.counter == 0 :
+        self.guess = element
+        self.counter = 1
+
+    elif self.guess == element:
+        self.counter += 1
+
+    else:
+        self.counter -= 1
+
+    majority_guess = self.get_majority()
+
+    print("element: {} , counter: {}, majority_guess: {}".format(self.guess, self.counter, majority_guess))
+
   # Gives the best guess of which of the elements seen so far make up the
   # majority of the elements in set of elements. If a majority element exists,
   # this algorithm will report it correctly. Otherwise, there is no guarantee
   # about the output.
   def get_majority(self):
     #YOUR CODE HERE
-    pass
+    if self.counter >= 0:
+        return self.guess
+    else:
+        return 'something went wrong'
